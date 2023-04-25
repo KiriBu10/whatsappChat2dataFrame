8fc465474f58bc7e15864eeb0ca708df8305cb9d
Kiran Busch <kiranbusch@t-online.de>
2023-04-25 12:24:22 +0200

    added datetime converter

--- whatsappchat2dataframe/chat2dataframe.py
+++ whatsappchat2dataframe/chat2dataframe.py
@@ -21,6 +21,7 @@ class Converter:
             pd.DataFrame: return a pandas DataFrame of the .txt file
         """
         self.DATE_TIME = re.compile(r"([\[]).*?([\]])")
+        self.DATE_TIME = re.compile(r"[\[]*[0-9]+[\./][0-9]+[\./][0-9]+..[0-9]+:[0-9]+[:]*([0-9]+)*[\]]*[-]*")
         self.AUTHOR = re.compile(r"((( [a-zA-ZöäüÖÄÜ]+)+):)")
         self.LTR = chr(8206)
         try:
@@ -39,14 +40,21 @@ class Converter:
         match = self.DATE_TIME.match(s)
         if match:
             g = match.group(0)
-            date = g[1:9]
-            time = g[11:-1]
+            date_time = g.replace('[','').replace(']','').replace(' ','').split(',')
             author = re.search(self.AUTHOR, s).group(0)[1:-1]
             message = s.split(': ')[1]
-            return pd.to_datetime(f'{date}-{time}', format="%d.%m.%y-%H:%M:%S"), author, message.replace("\n", "")
+            return self.__try_parsing_date(date_time), author, message.replace("\n", "")
         
     def __chat2df(self,data):
         start_positions_date = [m.start() for m in re.finditer(self.DATE_TIME, data)]
         for start,end in tqdm(list(zip(start_positions_date[:-1],start_positions_date[1:])), desc='Read messages'):
             subdata = data[start:end]
             yield self.__to_pd_row(subdata.replace(self.LTR, ""))
+    
+    def __try_parsing_date(self,date_time):
+        for fmt in ("%d.%m.%y-%H:%M:%S", "%Y/%m/%d-%H:%M:%S", "%d/%m/%y-%H:%M"):
+            try:
+                return pd.to_datetime(f'{date_time[0]}-{date_time[1]}', format=fmt)
+            except ValueError:
+                pass
+        raise ValueError(f'no valid date format found for "{str(date_time[0])}-{str(date_time[1])}"')
